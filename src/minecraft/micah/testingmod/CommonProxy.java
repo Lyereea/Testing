822b4a3895e633c11142b0e2f3bdef4e717555df
Lyereea <micah@Bankston-Boys-iMac-2.local>
2014-01-02 15:07:09 -0700

    Revert "I hope this works..."
    This reverts commit 9b7bd9615bde81419a3c971103830bf0465f1b83.

--- src/minecraft/micah/testingmod/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package micah.testingmod;
-
-public class CommonProxy {
-	// Client stuff
-	public void registerRenderers() {
-		// Nothing here as the server doesn't render graphics or entities.
-	}
-}
